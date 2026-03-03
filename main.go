d9aae8d4b4e2bde10e262c6c274360eaac22659e
TonglengUnii <tongleng.l@unii.co.th>
2026-03-03 18:18:47 +0700

    add internal source

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"backend/configs"
 	"backend/routes"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -12,9 +13,10 @@ import (
 func main() {
 	app := fiber.New()
 
-	// 2. Add CORS Configuration
+	mainDNS := os.Getenv("MAIN_DNS")
+
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000, http://localhost:5173, http://localhost:3001",
+		AllowOrigins:     mainDNS + ", http://localhost:3000, http://localhost:5173, http://localhost:3001",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
 		AllowCredentials: true,
@@ -22,13 +24,10 @@ func main() {
 
 	app.Use(logger.New())
 
-	// 1. Database
 	configs.ConnectDB()
 	configs.RunMigrations()
 
-	// 2. Setup Routes
 	routes.Setup(app, configs.DB)
 
-	// 3. Start
 	app.Listen(":8080")
 }
